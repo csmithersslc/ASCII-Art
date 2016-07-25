1be487252ac847f7d17655f6b7edeab3f559b9a6
Christina Smithers <christina.smithers@gmail.com>
2016-07-24 18:57:30 -0600

    Add files via upload

--- dbdemo.py
+++ dbdemo.py
@@ -50,4 +50,4 @@ class MainPage(Handler):
 			self.render_front(title, art, error)
 
 
-app = webapp2.WSGIApplication([('/', MainPage), 'ASCIIPage', Handler], debug=True)
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
